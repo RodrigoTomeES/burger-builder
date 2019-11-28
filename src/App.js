a1caf6ef193c03457cbc9a0cc037f27b117c2e4f
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-28 10:40:39 +0100

    Added builder burguer and dynamic ingredient component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
+
 function App() {
   return (
     <div>
-
+      <Layout>
+        <BurguerBuilder />
+      </Layout>
     </div>
   );
 }
