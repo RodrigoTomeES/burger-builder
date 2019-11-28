de9720df89fe6506ad44d08ab02796bef28223ab
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-28 11:17:04 +0000

    Updates

---
+++ precache-manifest.f85d6df3b41a0e1b16b09375d58c098f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a440592ca6bcc82b321365587fdc259b",
+    "url": "/burguer-builder/index.html"
+  },
+  {
+    "revision": "71dd90424a00ea294dfb",
+    "url": "/burguer-builder/static/css/main.b627fa54.chunk.css"
+  },
+  {
+    "revision": "01db3b3ac28cdcab84fc",
+    "url": "/burguer-builder/static/js/2.9a3c12b9.chunk.js"
+  },
+  {
+    "revision": "71dd90424a00ea294dfb",
+    "url": "/burguer-builder/static/js/main.d5027cae.chunk.js"
+  },
+  {
+    "revision": "0428e88e59b1b6a3870f",
+    "url": "/burguer-builder/static/js/runtime-main.b19d5187.js"
+  }
+]);
