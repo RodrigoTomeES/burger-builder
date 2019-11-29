76a7d44efc7bdb3b75ac71766376445ba633946f
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-29 23:43:30 +0100

    Updated nav links

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burguer Builder</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
+        <NavigationItem link="#" active>Burguer Builder</NavigationItem>
+        <NavigationItem link="#">Checkout</NavigationItem>
     </ul>
 );
 
