50ca9fab1d9ca00359413c3a251661d86dac4ab1
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-29 13:17:38 +0100

    Added toolbar and logo

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MEMI</div>
+        <Logo />
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
