7d8788425f86af3e295321704afc4ad024858e42
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-12-05 21:31:44 +0100

    Added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const app = (
-    <BrowserRouter>
+    <BrowserRouter basename='/burguer-builder/'>
         <App />
     </BrowserRouter>
 );
