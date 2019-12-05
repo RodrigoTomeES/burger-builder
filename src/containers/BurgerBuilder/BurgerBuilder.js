29906ffa3d1135999005b98e552d81f6515c7668
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-12-05 11:09:51 +0100

    Added routing to the app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -115,37 +115,47 @@ class BurguerBuilder extends Component {
     }
 
     purchaseContinuedHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Player 1',
-                address: {
-                    street: 'Test 1',
-                    zipCode: '12345',
-                    country: 'Spain'
-                },
-                email: 'player1@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Player 1',
+        //         address: {
+        //             street: 'Test 1',
+        //             zipCode: '12345',
+        //             country: 'Spain'
+        //         },
+        //         email: 'player1@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                console.log(response)
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-            })
-            .catch(error => {
-                console.log(error)
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         console.log(response)
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         });
+        //     })
+        //     .catch(error => {
+        //         console.log(error)
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         });
+        //     });
+        const queryParams = [];
+        for (let ingredient in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(ingredient) + '=' + encodeURIComponent(this.state.ingredients[ingredient]));
+        }
+        const queryString  = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: queryString
+        });
     }
 
     render() {
