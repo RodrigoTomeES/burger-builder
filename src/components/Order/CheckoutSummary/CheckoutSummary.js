e8df030573d4eb92f74ba116b17b7e81c7dfbdf3
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-12-05 14:00:18 +0100

    Fixed some responsive issues in Burger and CheckoutSummary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,7 +8,7 @@ const checkoutSummary = (props) => {
     return(
         <div className={classes.CheckoutSummary}>
             <h1>We hope it taste well!</h1>
-            <div style={{width: '100%', margin: 'auto'}}>
+            <div style={{width: '100%', margin: 'auto', display: 'flex'}}>
                 <Burger ingredients={props.ingredients} />
             </div>
             <Button btnType="Danger" clicked={props.checkoutCancelled}>CANCEL</Button>
