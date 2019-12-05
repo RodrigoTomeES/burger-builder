1b9817333e7ec5ef7da70450ccc7eca31aadc38a
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-12-05 10:11:10 +0000

    Updates

--- precache-manifest.3455cd2877907cf295ece70cded725b6.js
+++ precache-manifest.3455cd2877907cf295ece70cded725b6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dca2ce7408a4bfe51c3a83c7ddd09849",
+    "revision": "cf6a46e285df48bdd39235b29860d11d",
     "url": "/burguer-builder/index.html"
   },
   {
-    "revision": "0cdbdf710cf12f328614",
-    "url": "/burguer-builder/static/css/main.c05cfbcf.chunk.css"
+    "revision": "236a80ef717fef754d5e",
+    "url": "/burguer-builder/static/css/main.74c2c421.chunk.css"
   },
   {
-    "revision": "2609acac6b07f866c130",
-    "url": "/burguer-builder/static/js/2.971e21c1.chunk.js"
+    "revision": "74bafc70b4b8841c9dd6",
+    "url": "/burguer-builder/static/js/2.2978c5d0.chunk.js"
   },
   {
-    "revision": "0cdbdf710cf12f328614",
-    "url": "/burguer-builder/static/js/main.97b54037.chunk.js"
+    "revision": "236a80ef717fef754d5e",
+    "url": "/burguer-builder/static/js/main.c8ba54d8.chunk.js"
   },
   {
     "revision": "0428e88e59b1b6a3870f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burguer-builder/precache-manifest.5cb8977624b28722256bf4eec7f602e0.js"
+  "/burguer-builder/precache-manifest.3455cd2877907cf295ece70cded725b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burguer-builder/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
