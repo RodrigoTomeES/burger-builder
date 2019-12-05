fa02723772b68aeb855e2177de73cbc97b1ba574
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-12-05 13:26:25 +0100

    Added orders page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.module.css';
+
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients: Salad</p>
+        <p>Price: <strong>5€</strong></p>
+    </div>
+);
+
+export default order;
